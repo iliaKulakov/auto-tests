c9512161682efb82d90bd711593fbbfefad6ecda
RInat Muhamedgaliev <rinat.muhamedgaliev@gmail.com>
2018-08-01 12:14:38 +0300

    Fixed run tests from crossplatform solution Fixed maven structure

---
+++ src/test/java/io/github/maventest/tests/GoogleSearchTest.java
@@ -0,0 +1,43 @@
+package io.github.maventest.tests;
+
+import io.github.maventest.pageObjects.GooglePage;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import ru.stqa.selenium.factory.WebDriverPool;
+
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+public class GoogleSearchTest {
+
+    private WebDriver driver;
+
+    @AfterAll
+    public static void stopAllBrowsers() {
+        WebDriverPool.DEFAULT.dismissAll();
+    }
+
+    @BeforeEach
+    public void startBrowser() {
+        driver = WebDriverPool.DEFAULT.getDriver(new ChromeOptions());
+    }
+
+    @Test
+    public void test() {
+        doSomething();
+    }
+
+    private void doSomething() {
+        driver.get("https://www.google.com");
+
+        GooglePage googlePage = new GooglePage(driver);
+
+        assertNotNull(googlePage.getLogo(), "Can't find logo element");
+    }
+}
+
+
+
+
