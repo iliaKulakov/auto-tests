3fce2f6867e19801d5e70faefb1439abaa6e17c7
ilia <38052676+iliaKulakov@users.noreply.github.com>
2018-08-03 07:16:05 +0300

    Change code in BeforeEach Annotaion

--- src/test/java/io/github/maventest/tests/GoogleSearchTest.java
+++ src/test/java/io/github/maventest/tests/GoogleSearchTest.java
@@ -5,8 +5,12 @@ import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 import ru.stqa.selenium.factory.WebDriverPool;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
 
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 
@@ -19,9 +23,23 @@ public class GoogleSearchTest {
         WebDriverPool.DEFAULT.dismissAll();
     }
 
+   // @BeforeEach
+   // public void startBrowser() {
+   //     driver = WebDriverPool.DEFAULT.getDriver(new ChromeOptions());
+   // }
+
     @BeforeEach
-    public void startBrowser() {
-        driver = WebDriverPool.DEFAULT.getDriver(new ChromeOptions());
+    public void setUp() {
+        InputStream fis;
+        Properties properties = new Properties();
+        try {
+            fis = this.getClass().getClassLoader().getResourceAsStream("application.properties");
+            properties.load(fis);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        System.setProperty("webdriver.chrome.driver", properties.getProperty("webdriver.path"));
+        driver = new ChromeDriver();
     }
 
     @Test
